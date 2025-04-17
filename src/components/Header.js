d1197496645b9af6a246f4c22cfe70330243f897
esmael <esmaelhussenabdu@gmail.com>
2025-04-17 19:48:35 +0300

    Edit header navigations

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,15 @@ import logo from '../assets/logo.png';
 function Header() {
   return (
     <nav className="w-screen bg-white p-2 flex items-center justify-around">
-      <div className="flex items-center gap-8">
-        <img className="w-16" src={logo} alt="logo" />
-        <h1 className="font-bold">Space Travelers&apos; Hub</h1>
+      <div className="flex items-center gap-6">
+        <img className="w-20" src={logo} alt="logo" />
+        <h1 className="text-3xl font-bold">Space Travelers&apos; Hub</h1>
       </div>
-      <ul className="flex items-center list-none gap-8">
+      <ul className="flex items-center gap-6">
         <li>
           <NavLink
             to="/"
             className="text-lg text-sky-500 focus:underline"
-            end
           >
             Rockets
           </NavLink>
@@ -26,6 +25,7 @@ function Header() {
             Missions
           </NavLink>
         </li>
+        |
         <li>
           <NavLink
             to="/profile"
